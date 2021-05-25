78f92c39e94ae34d1122bf53d35ea54b8edfe29c
sumon <64481672+S8m6n2o7a@users.noreply.github.com>
2021-05-25 11:29:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+#ADD
+def add(n1, n2):
+  return n1 + n2
+
+#SUBTRACT
+def subtract(n1, n2):
+  return n1 - n2
+
+#MULTIPLY
+def multiply(n1, n2):
+  return n1 * n2
+
+#Divide
+def divide(n1, n2):
+  return n1 / n2
 
